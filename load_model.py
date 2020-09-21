06dd9f866357006ee613dd352016e80d80e8736b
Aaron Kutzer <aaron.kutzer@hotmail.de>
2020-09-21 11:54:19 +0200

    New model loading finally

--- load_model.py
+++ load_model.py
@@ -1,8 +1,9 @@
 import torch
 
+print(torch.hub.list('akutzer/mobilenetv3', force_reload=True))
 model = torch.hub.load('akutzer/mobilenetv3', 'mobilenet_v3', pretrained=False,
                        architecture="large", num_classes=420)
-print(model)
+#print(model)
 print("Param:", sum(p.numel() for p in model.parameters()))
 
 
@@ -10,5 +11,5 @@ model = torch.hub.load('akutzer/mobilenetv3', 'mobilenet_v3_segmentation',
                        pretrained=False, architecture="small", head="aspp",
                        shallow_stride=8, deep_stride=16,
                        out_c=20, head_c=128, width_mult=1.0)
-print(model)
+#print(model)
 print("Param:", sum(p.numel() for p in model.parameters()))
